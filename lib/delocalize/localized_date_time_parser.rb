ee35fac7b56c689f4cb3b2811b73a635ef04e96f
Brenton 'B-Train' Fletcher <i@bloople.net>
2011-11-01 13:14:58 +1100

    Cache date formats based on locale; Only use input formats if supplied

--- lib/delocalize/localized_date_time_parser.rb
+++ lib/delocalize/localized_date_time_parser.rb
@@ -61,7 +61,17 @@ module Delocalize
       def input_formats(type)
         # Date uses date formats, all others use time formats
         type = type == Date ? :date : :time
-        (@input_formats ||= {})[type] ||= I18n.t(:"#{type}.formats").slice(*I18n.t(:"#{type}.input.formats")).values
+        @input_formats ||= {}
+        @input_formats[I18n.locale] ||= {}
+
+        formats = I18n.t(:"#{type}.formats")
+        begin
+          input_formats = I18n.t!(:"#{type}.input.formats")
+          formats = formats.slice(*input_formats)
+        rescue I18n::MissingTranslationData
+        end
+
+        @input_formats[I18n.locale][type] ||= formats.values
       end
 
       def apply_regex(format)
